ee3aa0e98124852e7be2d69cf8faf7d1ae4b7cc6
mckellardw <dwm269@cornell.edu>
2023-04-27 11:49:55 -0400

    `h5_to_list_parallel.py` update

--- scripts/py/h5_to_list_parallel.py
+++ scripts/py/h5_to_list_parallel.py
@@ -4,7 +4,8 @@ import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 
 def seqDecode(seqInt, seqLen):
-    ATCG_BASES = ['A', 'C', 'T', 'G']
+    # ATCG_BASES = ['A', 'C', 'T', 'G']
+    ATCG_BASES = ['T', 'G', 'A', 'C'] # use complement to decode
     seqs = ""
     for i in range(seqLen):
         tint = (seqInt >> (i * 2)) & 3
@@ -12,11 +13,13 @@ def seqDecode(seqInt, seqLen):
     return seqs
 
 def process_chunk(chunk, progress, seq_length, verbose):
-    lines_per_update = 10000000
+    lines_per_update = 20000000
     results = []
     for row_idx, row in enumerate(chunk):
         for col_idx, item in enumerate(row):
             int_item = int(item)  # Convert item to integer if necessary
+            if int_item == 0:  # Skip if the integer is zero
+                continue
             nucleotide_pairs = seqDecode(int_item, seq_length)
             results.append(f"{nucleotide_pairs}\t{row_idx}\t{col_idx}\n")
             progress[0] += 1
@@ -24,49 +27,40 @@ def process_chunk(chunk, progress, seq_length, verbose):
                 print(f"Processed {progress[0]} lines")
     return results
 
-
-def process_group(group, output_file, seq_length=25, n_chunks=4, verbose=False):
-    progress = [0]  # Using a list to allow the integer to be mutable inside the process_chunk function
-
+def process_group(group, output_file, seq_length, n_threads, verbose):
     for key, value in group.items():
         if isinstance(value, h5py.Dataset):
-            dataset_array = np.array(value)
-            chunk_size = len(dataset_array) // n_chunks
+            dataset_array = np.array(value)  # Convert the dataset to a NumPy array
+            chunk_size = dataset_array.shape[0] // n_threads
+            chunks = [dataset_array[i:i + chunk_size] for i in range(0, dataset_array.shape[0], chunk_size)]
+            progress = [0]
+            results = []
 
-            with ThreadPoolExecutor() as executor:
-                futures = []
-                for chunk_start in range(0, len(dataset_array), chunk_size):
-                    chunk_end = min(chunk_start + chunk_size, len(dataset_array))
-                    chunk = dataset_array[chunk_start:chunk_end]
-                    futures.append(executor.submit(process_chunk, chunk, progress, seq_length, verbose))
+            with ThreadPoolExecutor(max_workers=n_threads) as executor:
+                futures = [executor.submit(process_chunk, chunk, progress, seq_length, verbose) for chunk in chunks]
+                for future in as_completed(futures):
+                    results.extend(future.result())
 
-                for future in futures:
-                    chunk_results = future.result()
-                    for result in chunk_results:
-                        output_file.write(result)
+            with open(output_file, "a") as out:
+                for result in results:
+                    out.write(result)
 
         elif isinstance(value, h5py.Group):
-            process_group(value, output_file, seq_length, n_chunks, verbose)
+            process_group(value, output_file, seq_length, n_threads, verbose)
 
-def main(input_file, output_file, seq_length=25, n_chunks=4, verbose=False):
-    with h5py.File(input_file, "r") as f, open(output_file, "w") as out:
-        process_group(f, out, seq_length, n_chunks, verbose)
+def main(input_file, output_file, seq_length=25, n_threads=16, verbose=False):
+    with h5py.File(input_file, "r") as f:
+        process_group(f, output_file, seq_length, n_threads, verbose)
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: python h5_to_list.py input_file output_file [--seq-length N] [--chunks N] [--verbose]")
+        print("Usage: python h5_to_list.py input_file output_file [seq_length] [n_threads] [--verbose]")
         sys.exit(1)
 
-    seq_length = 25
-    n_chunks = 4
-    verbose = False
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    seq_length = int(sys.argv[3]) if len(sys.argv) > 3 else 25
+    n_threads = int(sys.argv[4]) if len(sys.argv) > 4 else 16
+    verbose = "--verbose" in sys.argv
 
-    for idx, arg in enumerate(sys.argv):
-        if arg == '--seq-length' and idx + 1 < len(sys.argv):
-            seq_length = int(sys.argv[idx + 1])
-        elif arg == '--chunks' and idx + 1 < len(sys.argv):
-            n_chunks = int(sys.argv[idx + 1])
-        elif arg == '--verbose':
-            verbose = True
-
-    main(sys.argv[1], sys.argv[2], seq_length, n_chunks, verbose)
+    main(input_file, output_file, seq_length, n_threads, verbose)
